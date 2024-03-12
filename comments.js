59e1bc20448e6bba8a9182638df501a64501275c
Yuxin Tang <petertangrice@gmail.com>
2024-03-12 20:12:45 +0000

    fix third commit

--- comments.js
+++ comments.js
@@ -1,49 +1,36 @@
-// create web server using express
-// import express module
-var express = require('express');
-// create an express application
-var app = express();
-// import the file system module
-var fs = require('fs');
-// import the path module
-var path = require('path');
-// import the body-parser module
-var bodyParser = require('body-parser');
-// import the comments module
-var comments = require('./comments');
-// import the comments file
-var commentsFile = path.join(__dirname, 'comments.json');
-// use the body-parser module
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-// set the port number
-app.set('port', (process.env.PORT || 3000));
-// set the path to the public folder
-app.use('/', express.static(path.join(__dirname, 'public')));
-// get the comments
-app.get('/api/comments', function(req, res) {
-  fs.readFile(commentsFile, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    res.json(JSON.parse(data));
-  });
-});
-// add the comments
-app.post('/api/comments', function(req, res) {
-  fs.readFile(commentsFile, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    var comments = JSON.parse(data);
-    var newComment = {
-      id: Date.now(), author: req.body.author, text: req.body.text,
-    }; comments.push(newComment); fs.writeFile(commentsFile, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.json(comments);
-    } ); } ); } );
+// create web server with express 
+const express = require('express');
+const router = express.Router();
+const {Comment} = require('../models/Comment');
+
+//=================================
+//             Comment
+//=================================
+
+// save comment to database
+router.post('/saveComment', (req, res) => {
+    const comment = new Comment(req.body)
+
+    comment.save((err, comment) => {
+        if(err) return res.json({success: false, err})
+
+        Comment.find({'_id': comment._id})
+            .populate('writer')
+            .exec((err, result) => {
+                if(err) return res.json({success: false, err})
+                res.status(200).json({success: true, result})
+            })
+    })
+})
+
+// get comments from database
+router.post('/getComments', (req, res) => {
+    Comment.find({"postId": req.body.videoId})
+        .populate('writer')
+        .exec((err, comments) => {
+            if(err) return res.status(400).send(err)
+            res.status(200).json({success: true, comments})
+        })
+})
+
+module.exports = router;
